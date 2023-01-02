f4559f0a18b8b044102a0e9eb1472875e1a03f78
silver0hatim <113882565+silver0hatim@users.noreply.github.com>
2023-01-02 15:12:11 +0100

    Update tftp_server.c

--- Core/Src/tftp_server.c
+++ Core/Src/tftp_server.c
@@ -7,7 +7,16 @@
 
 #include <tftp_server.h>
 enum tftp_err tftp_server_init(void){
+  MX_LWIP_Init();
+  tftp_file_init();
 
+  struct netconn *conn = netconn_new(NETCONN_UDP);
+  if(netconn_bind(conn, NULL, 69) == ERR_OK){
+		return TFTPSERV_OK;
+  }
+	else{
+		return TFTPSERV_ERR;
+  }
 }
 void tftp_server_run(void ){
 
