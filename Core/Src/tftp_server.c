918147218d52cade0ff114a56f61bbe4e53c7f36
Legend Gaf <21614207@etu.unicaen.fr>
2023-01-02 14:49:45 +0100

    server.c added

---
+++ Core/Src/tftp_server.c
@@ -0,0 +1,14 @@
+/*
+ * tftp_server.c
+ *
+ *  Created on: 2 janv. 2023
+ *      Author: ayman
+ */
+
+#include <tftp_server.h>
+enum tftp_err tftp_server_init(void){
+
+}
+void tftp_server_run(void ){
+
+}
